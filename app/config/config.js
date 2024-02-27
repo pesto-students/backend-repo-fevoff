cb4b3eaaafa58df03a8d2bfd039fa2ef2ddc62a6
rjshah1902 <rjshah1902@gmail.com>
2024-02-27 22:50:55 +0530

    changes added

--- app/config/config.js
+++ app/config/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    CONFIG_URL: process.env.API_LINK,
+    CONFIG_URL: "mongodb+srv://fevoff:Rahul5057@fevoff.oocq6mx.mongodb.net/fevoff?retryWrites=true&w=majority&appName=Fevoff",
 };
