4f1cd37c335a99f8b04e08e90210532884bfbe97
rjshah1902 <rjshah1902@gmail.com>
2024-03-01 09:32:51 +0530

    brands model changes

--- app/model/brands.model.js
+++ app/model/brands.model.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
 
 const brands = mongoose.Schema({
-    name: String,
-    brand_slug: String,
-    show_hide: Number,
+    name: {
+        type: String,
+        required: [true, "Please Provide Brand Name"],
+    },
+    brand_slug: {
+        type: String,
+        required: [true, "Please Provide Brand Name"],
+    },
+    show_hide: {
+        type: Boolean,
+        default: true,
+    },
     brand_image: String,
     status: Number,
 }, { timestamps: true });
 
-module.exports = mongoose.model("brands", brands);
+module.exports = mongoose.model("brand", brands);
